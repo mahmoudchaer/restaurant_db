835dcad642cf6f111d2e41f84b0aeaa4b17a3a83
Mahmoud Chaer <mmc55@mail.aub.edu>
2024-11-18 01:43:06 +0200

    Update create_tables.sql

--- SQL/create_tables.sql
+++ SQL/create_tables.sql
@@ -1,232 +1,206 @@
---Mahmoud
+--mahmoud
 
-CREATE TABLE Customer (
-    Customer_ID id_type PRIMARY KEY,
-    Address address_type,
-    -- Avg_Rating rating_type, --derived attribute
-    Points INT ,
-    cust_Name name_type  not null,
-    Phone_Number phone_type,
-    Email email_type,
-    Table_ID INT, -- Foreign key references id of table
-    CONSTRAINT Phone_email_constraint_cust UNIQUE (Phone_Number, Email)
+create table customer (
+    customer_id id_type primary key,
+    address address_type,
+    -- avg_rating rating_type, --derived attribute
+    points int,
+    cust_name name_type not null,
+    phone_number phone_type,
+    email email_type,
+    table_id int, -- foreign key references id of table
+    constraint phone_email_constraint_cust unique (phone_number, email)
 );
 
-Create table HR(
-
-	employee_id id_type Primary key,
-	address address_type,
-	hr_name name_type not null,
-	salary money_type not null,
-	hr_role VARCHAR(50) not null,
-	email email_type not null,
-	phone_number phone_type not null,
-	
-	CONSTRAINT Phone_email_constraint_hr UNIQUE (Phone_Number, Email)
-
-
+create table hr (
+    employee_id id_type primary key,
+    address address_type,
+    hr_name name_type not null,
+    salary money_type not null,
+    hr_role varchar(50) not null,
+    email email_type not null,
+    phone_number phone_type not null,
+    constraint phone_email_constraint_hr unique (phone_number, email)
 );
 
-CREATE TABLE Payment_Method (
-    Payment_ID id_type  PRIMARY KEY,
-    Payment_Type VARCHAR(50) ,
-    Customer_ID id_type 
-    -- FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID) on delete cascade
+create table payment_method (
+    payment_id id_type primary key,
+    payment_type varchar(50),
+    customer_id id_type
+    -- foreign key (customer_id) references customer(customer_id) on delete cascade
 );
 
-CREATE TABLE Review (
-    Review_ID id_type , 
-    Customer_ID id_type, 
-    Rating rating_type  not null,
-    Description description_type ,
-    constraint pk_comp_cust_review PRIMARY KEY (Review_ID, Customer_ID)
-    -- FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID) on delete cascade
+create table review (
+    review_id id_type, 
+    customer_id id_type, 
+    rating rating_type not null,
+    description description_type,
+    constraint pk_comp_cust_review primary key (review_id, customer_id)
+    -- foreign key (customer_id) references customer(customer_id) on delete cascade
 );
 
-CREATE TABLE Image_Review (
-    Image_ID id_type  PRIMARY KEY,
-    Image image_type  not null, 
-    Review_Number INT,
-    Customer_ID id_type 
-    -- FOREIGN KEY (Review_Number, Customer_ID) REFERENCES Review(Review_ID, Customer_ID) on delete cascade
+create table image_review (
+    image_id id_type primary key,
+    image image_type not null, 
+    review_number int,
+    customer_id id_type 
+    -- foreign key (review_number, customer_id) references review(review_id, customer_id) on delete cascade
 );
 
-Create table Ingredient (
-	
-	inventory_id id_type primary key,
-	minimum_quantity quantity_type  not null,
-	--price money_type , --derived attribute
-	stock_qty quantity_type  not null,
-	ingr_name name_type  not null
-
+create table ingredient (
+    inventory_id id_type primary key,
+    minimum_quantity quantity_type not null,
+    --price money_type , --derived attribute
+    stock_qty quantity_type not null,
+    ingr_name name_type not null
 );
 
-create table meal(
-	meal_name name_type  primary key,
-	cost_meal money_type not null,
-	recipe description_type not null,
-	price money_type  not null,
-	category varchar (50) not null,
-	description description_type  not null
-	--avg_cook_time time, --derived
-
+create table meal (
+    meal_name name_type primary key,
+    cost_meal money_type not null,
+    recipe description_type not null,
+    price money_type not null,
+    category varchar(50) not null,
+    description description_type not null
+    --avg_cook_time time, --derived
 );
 
-create table image_meal(
-	image_id id_type  primary key,
-	image image_type  not null,
-	meal_name name_type 
-    -- FOREIGN KEY (meal_name) REFERENCES meal(meal_name)
-
-
+create table image_meal (
+    image_id id_type primary key,
+    image image_type not null,
+    meal_name name_type 
+    -- foreign key (meal_name) references meal(meal_name)
 );
 
 -- omar
 
-CREATE TABLE tables (
-    table_ID id_type PRIMARY KEY,         
-    seat_capacity INT NOT NULL CHECK (seat_capacity>=0),          
-    availability BOOLEAN NOT NULL,
-	waiter_id id_type --fk to waiter
+create table tables (
+    table_id id_type primary key,         
+    seat_capacity int not null check (seat_capacity>=0),          
+    availability boolean not null,
+    waiter_id id_type --fk to waiter
 );
 
-
-CREATE TABLE menu (
-    menu_ID id_type PRIMARY KEY,         
-    start_time time_type NOT NULL,           
-    end_time time_type NOT NULL,             
-    category VARCHAR(255) NOT NULL,      
+create table menu (
+    menu_id id_type primary key,         
+    start_time time_type not null,           
+    end_time time_type not null,             
+    category varchar(255) not null,      
     image image_type                        
 );
 
-
-CREATE TABLE menu_day (
-    menu_ID id_type NOT NULL,               
-    day VARCHAR(15) NOT NULL,           
-    PRIMARY KEY (menu_ID, day)      
-    -- FOREIGN KEY (menu_ID) REFERENCES menu(menu_ID) ON DELETE CASCADE               
+create table menu_day (
+    menu_id id_type not null,               
+    day varchar(15) not null,           
+    primary key (menu_id, day)      
+    -- foreign key (menu_id) references menu(menu_id) on delete cascade               
 );
 
-CREATE TABLE orders (
-    order_ID id_type PRIMARY KEY,     
-    date DATE NOT NULL,        
-    status VARCHAR(20) NOT NULL,     
-    priority VARCHAR(10),     
-    customer_id id_type, -- fk customer_ID to customer
-    delivery_driver_id id_type --fk delivery_driver to delivery driver
+create table orders (
+    order_id id_type primary key,     
+    date date not null,        
+    status varchar(20) not null,     
+    priority varchar(10),     
+    customer_id id_type, -- fk customer_id to customer
+    delivery_driver_id id_type --fk delivery_driver to delivery driver
 );
 
-
-CREATE TABLE chef (
-    employee_id id_type PRIMARY KEY,      
-    chef_role VARCHAR(50) NOT NULL,          
-    salary money_type NOT NULL,     
-    hire_date DATE,            
-    CV description_type,                            
+create table chef (
+    employee_id id_type primary key,      
+    chef_role varchar(50) not null,          
+    salary money_type not null,     
+    hire_date date,            
+    cv description_type,                            
     supervisor_id id_type,
     works_in name_type  --fk works_in to kitchen_station
-    -- FOREIGN KEY (supervisor_id) REFERENCES chef(employee_id) ON DELETE SET NULL               
+    -- foreign key (supervisor_id) references chef(employee_id) on delete set null               
 );
 
-CREATE TABLE waiter (
-    employee_id id_type PRIMARY KEY,      
-    waiter_role VARCHAR(50) NOT NULL,           
-    waiter_name name_type NOT NULL,          
-    CV description_type,                             
-    salary money_type NOT NULL,      
-    hire_date DATE NOT NULL,            
+create table waiter (
+    employee_id id_type primary key,      
+    waiter_role varchar(50) not null,           
+    waiter_name name_type not null,          
+    cv description_type,                             
+    salary money_type not null,      
+    hire_date date not null,            
     supervisor_id id_type                 
-    -- FOREIGN KEY (supervisor_id) REFERENCES waiter(employee_id) ON DELETE SET NULL                         
-);
---Hussein
-
-CREATE TABLE Supplier (
-	SupplierID id_type NOT NULL, 
-	NameSupp name_type NOT NULL,
-	Address address_type NOT NULL,
-	PhoneNumber phone_type NOT NULL,
-	Email email_type NOT NULL,
-	
-	CONSTRAINT US_constraint UNIQUE (PhoneNumber, Email), -- Constraint 	Unique Supplier
-	PRIMARY KEY (SupplierID)
+    -- foreign key (supervisor_id) references waiter(employee_id) on delete set null              
 );
 
-CREATE TABLE Product (
-	ProductID id_type NOT NULL,
-	ProductName name_type NOT NULL,
-	SupplierID id_type NOT NULL,
+--hussein
 
-	PRIMARY KEY (ProductID),
-	FOREIGN KEY (SupplierID) REFERENCES Supplier(SupplierID) ON DELETE CASCADE
+create table supplier (
+    supplierid id_type not null, 
+    namesupp name_type not null,
+    address address_type not null,
+    phonenumber phone_type not null,
+    email email_type not null,
+    constraint us_constraint unique (phonenumber, email), -- constraint unique supplier
+    primary key (supplierid)
 );
 
-
-CREATE TABLE DeliveryDriver(
-	EmployeeID id_type NOT NULL,
-	DriverName name_type NOT NULL,
-	ContactInfo phone_type NOT NULL,
-	VehicleType VARCHAR(10) NOT NULL,
-	AverageRating rating_type,
-	WaiterContact id_type NOT NULL,
-
-	CONSTRAINT UDD_constraint UNIQUE (ContactInfo),  --Constraint Unique delivery driver
-	PRIMARY KEY (EmployeeID)
-	-- FK Waiter Contact references EmployeeID in waiter
+create table product (
+    productid id_type not null,
+    productname name_type not null,
+    supplierid id_type not null,
+    primary key (productid),
+    foreign key (supplierid) references supplier(supplierid) on delete cascade
 );
 
-CREATE TABLE AssignedLocations(
-	LocationID id_type NOT NULL,
-	Address address_type NOT NULL,
-	Delivery_EmployeeID id_type, --We could have a location not yet assigned to a driver or if the driver responsible for it just quit
-
-	CONSTRAINT Location_constraint UNIQUE (LocationID),   --Constraint Unique location ID
-	PRIMARY KEY (LocationID),
-	FOREIGN KEY (Delivery_EmployeeID) REFERENCES DeliveryDriver(EmployeeID) ON DELETE SET NULL
-	
+create table deliverydriver (
+    employeeid id_type not null,
+    drivername name_type not null,
+    contactinfo phone_type not null,
+    vehicletype varchar(10) not null,
+    averagerating rating_type,
+    waitercontact id_type not null,
+    constraint udd_constraint unique (contactinfo),  --constraint unique delivery driver
+    primary key (employeeid)
+    -- fk waiter contact references employeeid in waiter
 );
 
-
-CREATE TABLE Administration(
-	EmployeeID id_type NOT NULL,
-	AdmName name_type NOT NULL,
-	AdmRole VARCHAR(30), -- Could be NULL if newly hired, promoted or demoted/suspended
-	Salary money_type, --Could be NULL if newly hired, promoted or demoted/suspended
-	Address address_type NOT NULL, 
-	PhoneNumber phone_type NOT NULL,
-	Email email_type NOT NULL,
-
-	CONSTRAINT Phone_email_constraint_adm UNIQUE (PhoneNumber, Email),
-	PRIMARY KEY (EmployeeID)
-	--NO FK
+create table assignedlocations (
+    locationid id_type not null,
+    address address_type not null,
+    delivery_employeeid id_type, --we could have a location not yet assigned to a driver or if the driver responsible for it just quit
+    constraint location_constraint unique (locationid),   --constraint unique location id
+    primary key (locationid),
+    foreign key (delivery_employeeid) references deliverydriver(employeeid) on delete set null
 );
 
-
-
-CREATE TABLE KitchenStation (
-	StationName name_type NOT NULL,
-	Number_of_Chefs INTEGER, --Could be NULL if station newly created or about to be closed
-	Specialization VARCHAR(30) NOT NULL,
-	ManagerID id_type NOT NULL,
-
-
-	CONSTRAINT station_mngr_kitchen_unique UNIQUE (StationName, ManagerID),
-	PRIMARY KEY (StationName)
-	--FOREIGN KEY ManagerID REFERENCES Chef(EmployeeID) ON DELETE SET NULL
+create table administration (
+    employeeid id_type not null,
+    admname name_type not null,
+    admrole varchar(30), -- could be null if newly hired, promoted or demoted/suspended
+    salary money_type, --could be null if newly hired, promoted or demoted/suspended
+    address address_type not null, 
+    phonenumber phone_type not null,
+    email email_type not null,
+    constraint phone_email_constraint_adm unique (phonenumber, email),
+    primary key (employeeid)
+    --no fk
 );
 
+create table kitchenstation (
+    stationname name_type not null,
+    number_of_chefs integer, --could be null if station newly created or about to be closed
+    specialization varchar(30) not null,
+    managerid id_type not null,
+    constraint station_mngr_kitchen_unique unique (stationname, managerid),
+    primary key (stationname)
+    --foreign key managerid references chef(employeeid) on delete set null
+);
 
-
-CREATE TABLE EmergencyContact(
-	ContactName VARCHAR(25) NOT NULL,
-	Relation VARCHAR(20) NOT NULL,
-	Priority INTEGER NOT NULL,
-	Phone_Number INTEGER NOT NULL,
-	Dep_EmployeeID INTEGER NOT NULL,
-
-	--NO UNIQUE CONSTRAINT AS TWO EMPLOYEES CAN HAVE THE SAME EMERGENCY CONTACT, SAY IF THEY WERE RELATED 
-	--AND THE SAME EMPPLOYEE CAN HAVE MULTIPLE EMERGENCY CONTACTS
-	CONSTRAINT PK_constraint PRIMARY KEY (ContactName, dep_EmployeeID)
-	--FOREIGN KEY Dep_EmployeeID REFERENCES Waiter(EmployeeID) ON DELETE CASCADE,
-	--FOREIGN KEY Dep_EmployeeID REFERENCES DeliveryDriver(EmployeeID) ON DELETE CASCADE,
-	--FOREIGN KEY Dep_EmployeeID REFERENCES Chef(EmployeeID) ON DELETE CASCADE,
+create table emergencycontact (
+    contactname varchar(25) not null,
+    relation varchar(20) not null,
+    priority integer not null,
+    phone_number integer not null,
+    dep_employeeid integer not null,
+    --no unique constraint as two employees can have the same emergency contact, say if they were related 
+    --and the same employee can have multiple emergency contacts
+    constraint pk_constraint primary key (contactname, dep_employeeid)
+    --foreign key dep_employeeid references waiter(employeeid) on delete cascade,
+    --foreign key dep_employeeid references deliverydriver(employeeid) on delete cascade,
+    --foreign key dep_employeeid references chef(employeeid) on delete cascade,
 );
