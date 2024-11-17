165652a8ee73247cf13c24d5c1ad277919830bfc
Mahmoud Chaer <mmc55@mail.aub.edu>
2024-11-17 23:34:11 +0200

    domains

---
+++ SQL/domains.sql
@@ -0,0 +1,23 @@
+-- domains
+
+CREATE DOMAIN id_type AS INTEGER;
+
+CREATE DOMAIN address_type AS VARCHAR (255);
+
+CREATE DOMAIN email_type AS VARCHAR(50);
+
+CREATE DOMAIN money_type AS DECIMAL(10,2);
+
+CREATE DOMAIN rating_type AS DECIMAL(3 ,1);
+
+CREATE DOMAIN description_type AS CLOB;
+
+CREATE DOMAIN quantity_type AS INT;
+
+CREATE DOMAIN time_type AS TIMESTAMP;
+
+CREATE DOMAIN phone_type AS INTEGER;
+
+CREATE DOMAIN name_type AS VARCHAR(30);
+
+CREATE DOMAIN image_type AS BYTEA;
