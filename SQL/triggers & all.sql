72e704c0fe5418ffb5b24fc8a496d42a89c3111f
Mahmoud Chaer <mmc55@mail.aub.edu>
2024-11-18 22:36:10 +0200

    Update triggers & all.sql

--- SQL/triggers & all.sql
+++ SQL/triggers & all.sql
@@ -44,3 +44,35 @@ AFTER INSERT ON chef
 FOR EACH ROW
 EXECUTE FUNCTION insert_into_employee_base_for_chef();
 
+
+CREATE OR REPLACE FUNCTION insert_into_employee_base_for_driver()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO employee_base (employee_id, employee_type, delivery_driver_id)
+    VALUES (NEW.employee_id, 'delivery driver', NEW.employee_id);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER after_driver_insert
+AFTER INSERT ON delivery_driver
+FOR EACH ROW
+EXECUTE FUNCTION insert_into_employee_base_for_driver();
+
+
+CREATE OR REPLACE FUNCTION insert_into_employee_base_for_waiter()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO employee_base (employee_id, employee_type, waiter_id)
+    VALUES (NEW.employee_id, 'waiter', NEW.employee_id);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER after_waiter_insert
+AFTER INSERT ON waiter
+FOR EACH ROW
+EXECUTE FUNCTION insert_into_employee_base_for_waiter();
+
+
+
