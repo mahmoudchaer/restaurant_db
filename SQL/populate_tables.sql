fd8b922c54ffec46f28d0f6c81b3b8fd4d1fe55a
Mahmoud Chaer <mmc55@mail.aub.edu>
2024-11-18 18:23:58 +0200

    pop

---
+++ SQL/populate_tables.sql
@@ -0,0 +1,67 @@
+--meal
+
+INSERT INTO meal (meal_name, cost_meal, recipe, price, category, description) 
+VALUES ('Meal_1', 7.50, 'Recipe for Meal_1', 15.00, 'Category_A', 'Description of Meal_1');
+INSERT INTO meal (meal_name, cost_meal, recipe, price, category, description) 
+VALUES ('Meal_2', 6.00, 'Recipe for Meal_2', 12.00, 'Category_A', 'Description of Meal_2');
+INSERT INTO meal (meal_name, cost_meal, recipe, price, category, description) 
+VALUES ('Meal_3', 9.20, 'Recipe for Meal_3', 18.40, 'Category_A', 'Description of Meal_3');
+INSERT INTO meal (meal_name, cost_meal, recipe, price, category, description) 
+VALUES ('Meal_4', 8.70, 'Recipe for Meal_4', 17.40, 'Category_A', 'Description of Meal_4');
+INSERT INTO meal (meal_name, cost_meal, recipe, price, category, description) 
+VALUES ('Meal_5', 5.50, 'Recipe for Meal_5', 11.00, 'Category_A', 'Description of Meal_5');
+INSERT INTO meal (meal_name, cost_meal, recipe, price, category, description) 
+VALUES ('Meal_6', 6.80, 'Recipe for Meal_6', 13.60, 'Category_A', 'Description of Meal_6');
+INSERT INTO meal (meal_name, cost_meal, recipe, price, category, description) 
+VALUES ('Meal_7', 10.50, 'Recipe for Meal_7', 21.00, 'Category_A', 'Description of Meal_7');
+INSERT INTO meal (meal_name, cost_meal, recipe, price, category, description) 
+VALUES ('Meal_8', 7.00, 'Recipe for Meal_8', 14.00, 'Category_A', 'Description of Meal_8');
+INSERT INTO meal (meal_name, cost_meal, recipe, price, category, description) 
+VALUES ('Meal_9', 8.30, 'Recipe for Meal_9', 16.60, 'Category_A', 'Description of Meal_9');
+INSERT INTO meal (meal_name, cost_meal, recipe, price, category, description) 
+VALUES ('Meal_10', 9.00, 'Recipe for Meal_10', 18.00, 'Category_A', 'Description of Meal_10');
+
+--hr
+INSERT INTO hr (employee_id, address, hr_name, salary, hr_role, email, phone_number) 
+VALUES (11, 'HR Address 1', 'HR_11', 75000.50, 'Role_11', 'hr11@example.com', '543211');
+INSERT INTO hr (employee_id, address, hr_name, salary, hr_role, email, phone_number) 
+VALUES (12, 'HR Address 2', 'HR_12', 89000.75, 'Role_12', 'hr12@example.com', '543212');
+INSERT INTO hr (employee_id, address, hr_name, salary, hr_role, email, phone_number) 
+VALUES (13, 'HR Address 3', 'HR_13', 67000.25, 'Role_13', 'hr13@example.com', '543213');
+INSERT INTO hr (employee_id, address, hr_name, salary, hr_role, email, phone_number) 
+VALUES (14, 'HR Address 4', 'HR_14', 92000.80, 'Role_14', 'hr14@example.com', '543214');
+INSERT INTO hr (employee_id, address, hr_name, salary, hr_role, email, phone_number) 
+VALUES (15, 'HR Address 5', 'HR_15', 81000.40, 'Role_15', 'hr15@example.com', '543215');
+INSERT INTO hr (employee_id, address, hr_name, salary, hr_role, email, phone_number) 
+VALUES (16, 'HR Address 6', 'HR_16', 75000.20, 'Role_16', 'hr16@example.com', '543216');
+INSERT INTO hr (employee_id, address, hr_name, salary, hr_role, email, phone_number) 
+VALUES (17, 'HR Address 7', 'HR_17', 56000.90, 'Role_17', 'hr17@example.com', '543217');
+INSERT INTO hr (employee_id, address, hr_name, salary, hr_role, email, phone_number) 
+VALUES (18, 'HR Address 8', 'HR_18', 65000.10, 'Role_18', 'hr18@example.com', '543218');
+INSERT INTO hr (employee_id, address, hr_name, salary, hr_role, email, phone_number) 
+VALUES (19, 'HR Address 9', 'HR_19', 72000.30, 'Role_19', 'hr19@example.com', '543219');
+INSERT INTO hr (employee_id, address, hr_name, salary, hr_role, email, phone_number) 
+VALUES (20, 'HR Address 10', 'HR_20', 91000.60, 'Role_20', 'hr20@example.com', '5432110');
+
+--customer
+INSERT INTO customer (customer_id, address, points, cust_name, phone_number, email, table_id) 
+VALUES (1, 'Address 1', 2345, 'Customer_1', '123451', 'cust1@example.com', 5);
+INSERT INTO customer (customer_id, address, points, cust_name, phone_number, email, table_id) 
+VALUES (2, 'Address 2', 3456, 'Customer_2', '123452', 'cust2@example.com', 8);
+INSERT INTO customer (customer_id, address, points, cust_name, phone_number, email, table_id) 
+VALUES (3, 'Address 3', 1200, 'Customer_3', '123453', 'cust3@example.com', 3);
+INSERT INTO customer (customer_id, address, points, cust_name, phone_number, email, table_id) 
+VALUES (4, 'Address 4', 4590, 'Customer_4', '123454', 'cust4@example.com', 7);
+INSERT INTO customer (customer_id, address, points, cust_name, phone_number, email, table_id) 
+VALUES (5, 'Address 5', 1678, 'Customer_5', '123455', 'cust5@example.com', 2);
+INSERT INTO customer (customer_id, address, points, cust_name, phone_number, email, table_id) 
+VALUES (6, 'Address 6', 1890, 'Customer_6', '123456', 'cust6@example.com', 6);
+INSERT INTO customer (customer_id, address, points, cust_name, phone_number, email, table_id) 
+VALUES (7, 'Address 7', 2980, 'Customer_7', '123457', 'cust7@example.com', 1);
+INSERT INTO customer (customer_id, address, points, cust_name, phone_number, email, table_id) 
+VALUES (8, 'Address 8', 3470, 'Customer_8', '123458', 'cust8@example.com', 9);
+INSERT INTO customer (customer_id, address, points, cust_name, phone_number, email, table_id) 
+VALUES (9, 'Address 9', 1540, 'Customer_9', '123459', 'cust9@example.com', 4);
+INSERT INTO customer (customer_id, address, points, cust_name, phone_number, email, table_id) 
+VALUES (10, 'Address 10', 2030, 'Customer_10', '1234510', 'cust10@example.com', 10);
+
