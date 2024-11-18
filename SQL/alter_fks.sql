7ac5d68df011707e333b15c9a885bf2b85a23d26
HusseinMoukalled <ham69@mail.aub.edu>
2024-11-18 15:10:15 +0200

    Update alter_fks.sql

--- SQL/alter_fks.sql
+++ SQL/alter_fks.sql
@@ -47,3 +47,42 @@ foreign key (supervisor_id) references chef(employee_id) on delete set null;
 alter table chef 
 add constraint fk_chef_kitchen 
 foreign key (works_in) references kitchen_station (stationname);
+
+alter table product 
+add constraint fk_product_supplier 
+foreign key (supplier_id) references supplier(supplier_id ); 
+
+
+alter table assigned_locations 
+add constraint fk_assigned_locations_driver 
+foreign key (delivery_employee_id) references delivery_driver(employee_id ); 
+
+
+alter table delivery_driver 
+add constraint fk_driver_waiter 
+foreign key (waiter_contact) references waiter(employee_id );
+
+
+alter table delivery_driver 
+add constraint fk_driver_waiter 
+foreign key (waiter_contact) references waiter(employee_id );
+
+
+alter table emergency_contact 
+add constraint fk_emergency_waiter 
+foreign key (dep_employee_id) references waiter(employee_id );
+
+
+alter table emergency_contact 
+add constraint fk_emergency_driver 
+foreign key (dep_employee_id) references delivery_driver(employee_id );
+
+
+alter table emergency_contact 
+add constraint fk_emergency_chef 
+foreign key (dep_employee_id) references chef(employee_id );
+
+
+alter table kitcehen_station 
+add constraint fk_kitchen_chef 
+foreign key (manager_id) references chef(employee_id );
