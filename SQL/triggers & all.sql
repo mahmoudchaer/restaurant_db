b35104257ec016456be997400b86dd06b2422b20
Mahmoud Chaer <mmc55@mail.aub.edu>
2024-11-18 22:48:48 +0200

    Update triggers & all.sql

--- SQL/triggers & all.sql
+++ SQL/triggers & all.sql
@@ -1,32 +1,4 @@
 
-CREATE OR REPLACE FUNCTION calculate_avg_rating(customer_id_input id_type)
-RETURNS void AS $$
-BEGIN
-    UPDATE customer
-    SET avg_rating = (
-        SELECT COALESCE(AVG(r.rating), 0)
-        FROM review r
-        WHERE r.customer_id = customer_id_input
-    )
-    WHERE customer_id = customer_id_input;
-END;
-$$ LANGUAGE plpgsql;
-
-
-CREATE OR REPLACE FUNCTION trigger_update_avg_rating()
-RETURNS TRIGGER AS $$
-BEGIN
-    -- Call the function to update the avg_rating
-    PERFORM calculate_avg_rating(NEW.customer_id);
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER update_avg_rating_on_insert
-AFTER INSERT ON review
-FOR EACH ROW
-EXECUTE FUNCTION trigger_update_avg_rating();
-
 
 --employee base
 
