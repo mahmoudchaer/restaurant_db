69de765472e1610f1a6ffaeb734de49c4bb6618a
Mahmoud Chaer <mmc55@mail.aub.edu>
2024-11-18 14:12:22 +0200

    Update domains.sql

--- SQL/domains.sql
+++ SQL/domains.sql
@@ -6,7 +6,7 @@ CREATE DOMAIN address_type AS VARCHAR (255);
 
 CREATE DOMAIN email_type AS VARCHAR(50) CHECK ( position('@' in VALUE) > 0 AND position('.' in VALUE) > 0);
 
-CREATE DOMAIN money_type AS DECIMAL(10,2) CHECK (VALUE >= 0); 
+CREATE DOMAIN money_type AS DECIMAL(10,2) CHECK (VALUE >= 0) ; 
 
 CREATE DOMAIN rating_type AS DECIMAL(3 ,1) CHECK (VALUE >= 0);
 
@@ -16,9 +16,9 @@ CREATE DOMAIN quantity_type AS INT CHECK (VALUE >= 0);
 
 CREATE DOMAIN time_type AS TIMESTAMP;
 
-CREATE DOMAIN phone_type AS INTEGER ;
+CREATE DOMAIN phone_type AS INTEGER;
 
-CREATE DOMAIN name_type AS VARCHAR(30);
+CREATE DOMAIN name_type AS VARCHAR(30) not null;
 
 CREATE DOMAIN image_type AS BYTEA;
 
