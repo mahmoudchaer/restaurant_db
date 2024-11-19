442b2c22d5992e890aff8e3c20d018cc9b93b8a2
HusseinMoukalled <ham69@mail.aub.edu>
2024-11-19 17:17:09 +0200

    Update complex_queries.sql

--- SQL/complex_queries.sql
+++ SQL/complex_queries.sql
@@ -1,1 +1,11 @@
 --complex queries
+
+--get top 3 highest paid ROLES on average
+SELECT chef_role, sal
+FROM (
+    SELECT chef_role, SUM(salary) AS sal
+    FROM chef
+    GROUP BY chef_role
+) AS subquery
+ORDER BY sal DESC
+LIMIT 3;
