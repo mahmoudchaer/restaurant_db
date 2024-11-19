e764bff2e0ed827c7a7539e703e122c174120437
HusseinMoukalled <ham69@mail.aub.edu>
2024-11-19 16:53:40 +0200

    Update triggers & all.sql

--- SQL/triggers & all.sql
+++ SQL/triggers & all.sql
@@ -48,3 +48,27 @@ EXECUTE FUNCTION insert_into_employee_base_for_waiter();
 
 
 
+--Increment number of chefs by one each time a chef is added to a station
+CREATE OR REPLACE FUNCTION increment_chef_count_function()
+RETURNS TRIGGER AS $$
+BEGIN
+  -- Increment the number of chefs for the specified kitchen station based on station_name
+  UPDATE kitchen_station
+  SET number_of_chefs = number_of_chefs + 1
+  WHERE station_name = NEW.works_in;  -- Referring to the works_in attribute from the inserted row in chef table
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER increment_chef_count
+AFTER INSERT ON chef
+FOR EACH ROW
+EXECUTE FUNCTION increment_chef_count_function();
+
+
+
+
+
+
+
+
