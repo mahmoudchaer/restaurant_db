d49e565057471b5ce05e45c119ee97cca2d3fddf
HusseinMoukalled <ham69@mail.aub.edu>
2024-11-19 17:14:30 +0200

    Update triggers & all.sql

--- SQL/triggers & all.sql
+++ SQL/triggers & all.sql
@@ -1,6 +1,6 @@
 
 
---Increment number of chefs by one each time a chef is added to a station
+--Increment number of chefs in the appropritate station by one each time a chef is added to a station
 CREATE OR REPLACE FUNCTION increment_chef_count_function()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -18,7 +18,22 @@ FOR EACH ROW
 EXECUTE FUNCTION increment_chef_count_function();
 
 
+-- Decrement chef count in the appropriate station whenever a chef is deleted
+CREATE OR REPLACE FUNCTION decrement_chef_count_function()
+RETURNS TRIGGER AS $$
+BEGIN
+  -- Decrement the number of chefs for the specified kitchen station based on station_name
+  UPDATE kitchen_station
+  SET number_of_chefs = number_of_chefs - 1
+  WHERE station_name = OLD.works_in;  -- Referring to the works_in attribute from the deleted row in chef table
+  RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
 
+CREATE TRIGGER decrement_chef_count
+AFTER DELETE ON chef
+FOR EACH ROW
+EXECUTE FUNCTION decrement_chef_count_function();
 
 
 
