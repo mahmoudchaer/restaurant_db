9f12e79211563091707de3152d91e492b76cdc67
Mahmoud Chaer <mmc55@mail.aub.edu>
2024-11-19 18:18:00 +0200

    Update complex_queries.sql

--- SQL/complex_queries.sql
+++ SQL/complex_queries.sql
@@ -36,4 +36,37 @@ GROUP BY
 ORDER BY 
     month;
 
+-- profitability and salaries of by kitchen station
+WITH station_profit_salary AS (
+    SELECT 
+        ks.station_name,
+        SUM((me.price - me.cost_meal) * c.quantity) AS total_profit,
+        SUM(ch.salary) AS total_salaries
+    FROM 
+        kitchen_station ks
+    JOIN 
+        chef ch ON ks.station_name = ch.works_in
+    JOIN 
+        creates cr ON ch.employee_id = cr.chef_id
+    JOIN 
+        composed_of co ON cr.menu_id = co.menu_id
+    JOIN 
+        contain c ON co.meal_name = c.meal_name
+    JOIN 
+        meal me ON c.meal_name = me.meal_name
+    GROUP BY 
+        ks.station_name
+)
+SELECT 
+    station_name,
+    total_profit,
+    total_salaries,
+    (total_profit - total_salaries) AS net_profit,
+    ROUND((total_profit / NULLIF(total_salaries, 0)) * 100, 2) AS profit_to_salary_ratio
+FROM 
+    station_profit_salary
+ORDER BY 
+    net_profit DESC;
+
+
 
