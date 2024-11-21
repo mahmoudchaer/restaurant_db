b205110c680900c295e854907ce2b8320551e56b
Mahmoud Chaer <mmc55@mail.aub.edu>
2024-11-21 19:56:14 +0200

    Update domains.sql

--- SQL/domains.sql
+++ SQL/domains.sql
@@ -1,18 +1,18 @@
 -- domains
 
-CREATE DOMAIN id_type AS VARCHAR(10);
+CREATE DOMAIN id_type AS INTEGER CHECK (VALUE BETWEEN 0 AND 99999999);
 
 CREATE DOMAIN address_type AS VARCHAR (255);
 
-CREATE DOMAIN email_type AS VARCHAR(50);
+CREATE DOMAIN email_type AS VARCHAR(50) CHECK ( position('@' in VALUE) > 0 AND position('.' in VALUE) > 0);
 
-CREATE DOMAIN money_type AS DECIMAL(10,2);
+CREATE DOMAIN money_type AS DECIMAL(10,2) CHECK (VALUE >= 0) ; 
 
-CREATE DOMAIN rating_type AS DECIMAL(3 ,1);
+CREATE DOMAIN rating_type AS DECIMAL(3 ,1) CHECK (VALUE >= 0);
 
-CREATE DOMAIN description_type AS text;
+CREATE DOMAIN description_type AS TEXT;
 
-CREATE DOMAIN quantity_type AS INT;
+CREATE DOMAIN quantity_type AS INT CHECK (VALUE >= 0);
 
 CREATE DOMAIN time_type AS TIMESTAMP;
 
@@ -21,3 +21,6 @@ CREATE DOMAIN phone_type AS INTEGER;
 CREATE DOMAIN name_type AS VARCHAR(30);
 
 CREATE DOMAIN image_type AS BYTEA;
+
+CREATE DOMAIN employee_type AS VARCHAR(20) CHECK (VALUE IN ('chef', 'delivery driver', 'waiter'));
+
